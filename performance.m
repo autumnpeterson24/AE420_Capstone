08a8cc0fd9424e361e62631368a34dd81695fda3
Franny52165416 <franciscolaso@yahoo.com>
2025-10-04 18:19:04 -0700

    Update performance.m

--- performance.m
+++ performance.m
@@ -1,4 +1,4 @@
-function [TOFL, Climb, MaxAlt, Time] = performance(W, S, P,V, h);
+function [TOFL, Climb, MaxAlt, Time2] = performance(W, S, P,V, h, AR);
 % Input variables:
 %		W		        Weight of aircraft (lb)
 %		S		        Wing area (ft^2)
@@ -14,7 +14,7 @@ function [TOFL, Climb, MaxAlt, Time] = performance(W, S, P,V, h);
 %Constants
 R = 1716.0;	 % specific gas constant for air [(ft*lb) / (slug*Rankine)]
 g = 32.174;	 % acceleration due to gravity (ft/s^2)
-
+AR =AR;
 %Baseline Constants
 Cl_max = 1.4;
 Cdo = [0.0313 0.0322 0.0321]; % TO 5200 6000 ft
@@ -26,9 +26,9 @@ Pres = 2116 .* (T./ 518.6).^5.256;    % NASA
 rho = Pres ./ (R.*T);
 
 %Preliminary Calculations
-V = V*1.68781; %ft/s
+%V = V*1.68781; %ft/s
 T_W = .3;
-P = [T_W*W .86*T_W*W .84*T_W*W]; %lbf Roths T/W estimate
+% P = [T_W*W .86*T_W*W .84*T_W*W]; %lbf Roths T/W estimate
 D = (1/2).*rho.*V^2*S.*Cdo; %lbf
 Vavg = 0.7.*1.2.*sqrt(2.*W./(rho(1).*S.*Cl_max)); % average velocity
 Lavg=1.*0.5.*rho(1).*Vavg.^2.*S; % average lift
@@ -37,13 +37,18 @@ Lavg=1.*0.5.*rho(1).*Vavg.^2.*S; % average lift
 TOFL = (1.44*W^2)/(rho(1)*S*Cl_max*g*(P(1)-D(1)-.03*(W-Lavg)));
 
 % rate of climb (ft/min) (Bennett)
-Climb_TO = V*((P(1)-D(1))/W);
-Climb_Cruise = V*((P(2)-D(2))/W);
-Climb_MaxAlt = V*((P(3)-D(3))/W);
+% Climb_TO = V*((P(1)-D(1))/W);
+% Climb_Cruise = V*((P(2)-D(2))/W);
+% Climb_MaxAlt = V*((P(3)-D(3))/W);
+% Climb = Climb_Cruise;
+Climb_TO = V*((P-D)/W);
+Climb_Cruise = V*((P-D)/W);
+Climb_MaxAlt = V*((P-D)/W);
 Climb = Climb_Cruise;
 
 % max altitude (ft) Needs to find altitude where 100ft/min climb is acheived (Bennett)
 MaxAlt = 15000;
+% P_MaxAlt = 100*W/V/60 + D(3);
 P_MaxAlt = 100*W/V/60 + D(3);
 
 alts = linspace(0,40000,80);
@@ -74,23 +79,27 @@ end
 % flight time (hours) https://www.researchgate.net/publication/269567470_Range_and_Endurance_Estimates_for_Battery-Powered_Aircraft
 Rt = 1; %Battery hour rating (typically 1 according to Traub for small rechargable battery packs)
 n = 1.3; %Battery discharge parameter (taken from Traub's paper saying 1.3 is common for lithium polymer batteries, li ion is closer to 1.05 for reference according to AI)
-V = 11.11; %Battery Voltage (Payloads team's value)
+Vt = 11.11; %Battery Voltage (Payloads team's value)
 C = 56; %Battery capacity in amp hours (Guess based off of similar applications and prop teams estimate of 5000mAh)
-k = 3*Cdo(2)/(Cl_max^2); % Sovled from Cdo = (1/3)k*CL^2 
+%k = 3*Cdo(2)/(Cl_max^2); % Sovled from Cdo = (1/3)k*CL^2 
 
-Time = (Rt^(1-n)) * ((n_total*V*C)/((2/sqrt(rho*S))*(Cdo(2)^.25)*(2*W*sqrt(k/3))^1.5))^n; %hours
-Time2 = (Rt^(1-n)) * ((n_total*V*C)/(.5*rho*V^3*S*Cdo(2) + 2*W^2*k/(rho*V*S)))^n; %hours
+e0= 4.6 *(1-0.033 *AR^(0.53))*(1^(0.1))-3.3;
+
+k=1/(pi*e0*AR);
+
+Time = (Rt^(1-n)) * ((n_total*Vt*C)/((2/sqrt(rho*S))*(Cdo(2)^.25)*(2*W*sqrt(k/3))^1.5))^n; %hours max time 
+Time2 = (Rt^(1-n)) * ((n_total*Vt*C)/(.5*rho*V^3*S*Cdo(2) + 2*W^2*k/(rho*V*S)))^n; %hours cruise time
 
 %Code Outputs
-fprintf("Takeoff field length              = %4.2f (ft)\n",TOFL);
-fprintf("Climb rate at takeoff             = %4.2f (ft/s) or %4.2f (ft/min)\n", Climb_TO,Climb_TO*60);
-fprintf("Climb rate at cruise              = %4.2f (ft/s) or %4.2f (ft/min)\n", Climb_Cruise,Climb_Cruise*60);
-fprintf("Climb rate at max altitude        = %4.2f (ft/s) or %4.2f (ft/min)\n", Climb_MaxAlt,Climb_MaxAlt*60);
-fprintf("Thrust required for max altitude  = %4.2f (lbf)\n",P_MaxAlt);
-fprintf("Maximum Altitude                  = %4.4f (ft)\n",MaxAlt);
-fprintf("Max Endurance                     = %4.2f (h)\n",Time);
-fprintf("Cruise Endurance                  = %4.2f (h)\n",Time2);
-
+% fprintf("Takeoff field length              = %4.2f (ft)\n",TOFL);
+% fprintf("Climb rate at takeoff             = %4.2f (ft/s) or %4.2f (ft/min)\n", Climb_TO,Climb_TO*60);
+% fprintf("Climb rate at cruise              = %4.2f (ft/s) or %4.2f (ft/min)\n", Climb_Cruise,Climb_Cruise*60);
+% fprintf("Climb rate at max altitude        = %4.2f (ft/s) or %4.2f (ft/min)\n", Climb_MaxAlt,Climb_MaxAlt*60);
+% fprintf("Thrust required for max altitude  = %4.2f (lbf)\n",P_MaxAlt);
+% fprintf("Maximum Altitude                  = %4.4f (ft)\n",MaxAlt);
+% fprintf("Max Endurance                     = %4.2f (h)\n",Time);
+% fprintf("Cruise Endurance                  = %4.2f (h)\n",Time2);
+% fprintf("\n");
 %--------------------------------------------------------------------------
 %NOTE
 
@@ -100,5 +109,4 @@ fprintf("Cruise Endurance                  = %4.2f (h)\n",Time2);
 %provides the 900-1100 ft/min climb rates at various altitudes. GA
 %requirement's state a climb rate of 400-500 ft/min was our target which
 %seems impossible to achieve both requirements so the takeoff distance was
-
 %prioritized as we can out perform climb rates if needed
